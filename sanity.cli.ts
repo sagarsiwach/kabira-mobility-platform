51473c6d488485a538e4163c3261893393b3df50
sagarsiwach <sagarsiwach19@outlook.com>
2025-05-03 12:02:01 +0530

    fix auto build

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,15 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'vqqzvr1q',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  // Add the option below to use unattended mode and skip version prompts
+  ci: {
+    unattended: true,
+  },
 })
